afaf3ba835a46d3013a8593711eb6b19fb513139
ghoulsblade <ghoulsblade@schattenkind.net>
2012-03-17 00:07:37 +0100

    style and some cleanup

--- love.audio.js
+++ love.audio.js
@@ -1,9 +1,10 @@
 function Love_LoadAudio (G) {
-	G.str['love'].str['audio'] = lua_newtable();
+	var t = lua_newtable();
+	G.str['love'].str['audio'] = t;
 	
 	// love.audio.newSource(path)
-	G.str['love'].str['audio'].str['newSource']		= function (path) { MainPrint("audio.newSource called "+path); return lua_newtable(); }
+	t.str['newSource']		= function (path) { MainPrint("audio.newSource called "+path); return lua_newtable(); }
 	
 	// love.audio.play(sourceobj, number)
-	G.str['love'].str['audio'].str['play']			= function (src,num) { MainPrint("audio.play called"); }
+	t.str['play']			= function (src,num) { MainPrint("audio.play called"); }
 }
--- love.graphics.js
+++ love.graphics.js
@@ -1,16 +1,17 @@
 function Love_LoadGraphics (G) {
+	var t = lua_newtable();
 
-	G.str['love'].str['graphics'] = lua_newtable();
+	G.str['love'].str['graphics'] = t;
 	
 	// love.graphics.newImage(path)
-	G.str['love'].str['graphics'].str['newImage']	= function (path) { MainPrint("graphics.newImage called "+path); return lua_newtable(); }
+	t.str['newImage']	= function (path) { MainPrint("graphics.newImage called "+path); return lua_newtable(); }
 	
 	// love.graphics.setBackgroundColor(r,g,b)
-	G.str['love'].str['graphics'].str['setBackgroundColor']	= function (r,g,b) { MainPrint("graphics.setBackgroundColor called"); }
+	t.str['setBackgroundColor']	= function (r,g,b) { MainPrint("graphics.setBackgroundColor called"); }
 	
 	// love.graphics.setColor(r,g,b,a)
-	G.str['love'].str['graphics'].str['setColor']	= function (r,g,b,a) { MainPrint("graphics.setColor called"); }
+	t.str['setColor']	= function (r,g,b,a) { MainPrint("graphics.setColor called"); }
 	
 	//~ love.graphics.draw(drawable, x, y, r, sx, sy, ox, oy )
-	G.str['love'].str['graphics'].str['draw']	= function (drawable, x, y, r, sx, sy, ox, oy ) { MainPrint("graphics.draw called"); }
+	t.str['draw']		= function (drawable, x, y, r, sx, sy, ox, oy ) { MainPrint("graphics.draw called"); }
 }
