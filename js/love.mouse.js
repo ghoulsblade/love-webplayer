351506203f476cc6b3ce9d2533b9c1609d690bc8
Bart van Strien <bart@bartbes.ath.cx>
2012-03-24 12:05:34 +0100

    Implement most of love.mouse

--- js/love.mouse.js
+++ js/love.mouse.js
@@ -1,3 +1,28 @@
+var gMouseX = 0;
+var gMouseY = 0;
+var gMouseDown = [false, false, false];
+var gMouseButtonNames = ["left", "middle", "right"];
+
+//document.captureEvents(Event.MOUSEMOVE);
+document.onmousemove = function(e)
+{
+	var tempX = e.pageX;
+	var tempY = e.pageY;
+	if (tempX >= 0 && tempX < gScreenWidth)
+		gMouseX = tempX;
+	if (tempY >= 0 && tempY < gScreenHeight)
+		gMouseY = tempY;
+};
+document.onmousedown = function(e)
+{
+	gMouseDown[e.button] = true;
+	push_event("mousepressed", gMouseX, gMouseY, gMouseButtonNames[e.button]);
+};
+document.onmouseup = function(e)
+{
+	gMouseDown[e.button] = false;
+	push_event("mousereleased", gMouseX, gMouseY, gMouseButtonNames[e.button]);
+};
 
 /// init lua api
 function Love_Mouse_CreateTable (G) {
@@ -6,12 +31,12 @@ function Love_Mouse_CreateTable (G) {
 
 	G.str['love'].str['mouse'] = t;
 	
-	t.str['getPosition']	= function () { NotImplemented(pre+'getPosition'); return [0,0]; }
-	t.str['getX']			= function () { NotImplemented(pre+'getX'); return [0]; }
-	t.str['getY']			= function () { NotImplemented(pre+'getY'); return [0]; }
-	t.str['isDown']			= function () { return NotImplemented(pre+'isDown'); }
-	t.str['isGrabbed']		= function () { return NotImplemented(pre+'isGrabbed'); }
-	t.str['isVisible']		= function () { return NotImplemented(pre+'isVisible'); }
+	t.str['getPosition']	= function () { return [gMouseX, gMouseY]; }
+	t.str['getX']			= function () { return [gMouseX]; }
+	t.str['getY']			= function () { return [gMouseY]; }
+	t.str['isDown']			= function () { return [gMouseDown]; } // Only left clicks, sorry!
+	t.str['isGrabbed']		= function () { return [false];}
+	t.str['isVisible']		= function () { return [true]; }
 	t.str['setGrab']		= function () { return NotImplemented(pre+'setGrab'); }
 	t.str['setPosition']	= function () { return NotImplemented(pre+'setPosition'); }
 	t.str['setVisible']		= function () { return NotImplemented(pre+'setVisible'); }
