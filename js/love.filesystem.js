35087eefdf87b319c3c5007e45b10cab26da024f
Bart van Strien <bart@bartbes.ath.cx>
2012-03-25 15:25:05 +0200

    Add very basic filesystem support (read/write/exists) using LocalStorage

--- js/love.filesystem.js
+++ js/love.filesystem.js
@@ -7,13 +7,13 @@ function Love_Filesystem_CreateTable (G) {
 	G.str['love'].str['filesystem'] = t;
 	
 	t.str['enumerate']				= function () { return NotImplemented(pre+'enumerate'); }
-	t.str['exists']					= function () { return NotImplemented(pre+'exists'); }
+	t.str['exists']					= function () { return NotImplemented(pre+'exists (no LocalStorage)'); }
 	t.str['getAppdataDirectory']	= function () { return NotImplemented(pre+'getAppdataDirectory'); }
 	t.str['getLastModified']		= function () { return NotImplemented(pre+'getLastModified'); }
 	t.str['getSaveDirectory']		= function () { return NotImplemented(pre+'getSaveDirectory'); }
 	t.str['getUserDirectory']		= function () { return NotImplemented(pre+'getUserDirectory'); }
 	t.str['getWorkingDirectory']	= function () { return NotImplemented(pre+'getWorkingDirectory'); }
-	t.str['init']					= function () { return NotImplemented(pre+'init'); }
+	t.str['init']					= function () { }
 	t.str['isDirectory']			= function () { return NotImplemented(pre+'isDirectory'); }
 	t.str['isFile']					= function () { return NotImplemented(pre+'isFile'); }
 	t.str['lines']					= function () { return NotImplemented(pre+'lines'); }
@@ -21,9 +21,25 @@ function Love_Filesystem_CreateTable (G) {
 	t.str['mkdir']					= function () { return NotImplemented(pre+'mkdir'); }
 	t.str['newFile']				= function () { return NotImplemented(pre+'newFile'); }
 	t.str['newFileData']			= function () { return NotImplemented(pre+'newFileData'); }
-	t.str['read']					= function () { return NotImplemented(pre+'read'); }
+	t.str['read']					= function () { return NotImplemented(pre+'read (no LocalStorage)'); }
 	t.str['remove']					= function () { return NotImplemented(pre+'remove'); }
 	t.str['setIdentity']			= function () { return NotImplemented(pre+'setIdentity'); }
 	t.str['setSource']				= function () { return NotImplemented(pre+'setSource'); }
-	t.str['write']					= function () { return NotImplemented(pre+'write'); }
+	t.str['write']					= function (filename, data) { return NotImplemented(pre+"write (no LocalStorage)"); }
+
+	if (localStorage)
+	{
+		t.str['write']					= function (filename, data)
+		{
+			localStorage[filename] = data;
+		}
+		t.str['read']                                   = function (filename)
+		{
+			return [localStorage[filename]];
+		}
+		t.str['exists']                                 = function (filename)
+		{
+			return [localStorage[filename] != undefined];
+		}
+	}
 }
