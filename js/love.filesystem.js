2ae4f6137d5cc491e5ec272c0efa3c36187017a5
ghoulsblade <ghoulsblade@schattenkind.net>
2012-03-26 21:22:05 +0200

    love.filesystem : allow enumerate to work if filelist.txt is supplied in index.html body onload

--- js/love.filesystem.js
+++ js/love.filesystem.js
@@ -1,4 +1,70 @@
 var gFilesystemPrefix = "unnamed-";
+var gFilesystemEnumerateList;
+var gFilesystemEnumerateIsDirectory = {};
+var gFilesystemEnumerateIsFile = {};
+
+
+function LoveFSNormalizePath (path) {
+	if (path.length >= 2 && path.substring(0,2) == "./") path = path.substring(2);
+	if (path.length >= 1 && path.substring(0,1) == "/") path = path.substring(1);
+	if (path.length >= 1 && path.substring(path.length-1) == "/") path = path.substring(0,path.length-1);
+	return path;
+}
+
+/// call LoveFileList('filelist.txt') in index.html body onload to enable love.filesystem.enumerate
+/// newline separated file paths, e.g. linux commandline "find . > filelist.txt"
+function LoveFileList (url) {
+	MainPrint("LoveFileList",url);
+	UtilAjaxGet(url, function (contents) {
+		if (contents) {
+			gFilesystemEnumerateList = {};
+			var paths = contents.split("\n");
+			for (var i in paths) {
+				var path = LoveFSNormalizePath(paths[i]);
+				if (path == "" || path == "." || path == "..") continue;
+				var parts = path.split("/");
+				var basename = parts.pop();
+				var parentpath = parts.join("/");
+				var pathlist = gFilesystemEnumerateList[parentpath];
+				if (!pathlist) { pathlist = []; gFilesystemEnumerateList[parentpath] = pathlist; }
+				pathlist.push(basename);
+				gFilesystemEnumerateIsFile[path] = true; MainPrint("IsFile",path);
+				gFilesystemEnumerateIsDirectory[parentpath] = true;
+				gFilesystemEnumerateIsFile[parentpath] = false;
+			}
+		}
+	}, true);
+}
+
+// Returns a table with the names of files and subdirectories in the directory in an undefined order. 
+// example: "dira" contains 1 file (a.txt) and 2 subdirs (diraa,dirab) :   love.filesystem.enumerate("dira") (="dira/") = {"a.txt","diraa","dirab"}
+function LoveFilesystemEnumerate (path) {
+	if (!gFilesystemEnumerateList) return NotImplemented(pre+'enumerate (try index.html body onload : LoveFileList("filelist.txt") from "find . > filelist.txt")');
+	var res = lua_newtable();
+	if (path.substring(path.length - 1) == "/") path = path.substring(0,path.length - 1); // remove trailing /
+	path = LoveFSNormalizePath(path);
+	// TODO : evaluate ./ and ../ , js-regex ? 
+	var pathlist = gFilesystemEnumerateList[path];
+	if (pathlist) for (var i in pathlist) res.uints[parseInt(i)+1] = pathlist[i]; 
+	return [res];
+}
+
+function LoveFS_isDir (path) {
+	var res = gFilesystemEnumerateIsDirectory[LoveFSNormalizePath(path)] || (localStorage && (path.substr(path.length-1) == "/")); // Directory name
+	return res == true;
+}
+
+function LoveFS_isFile (path) {
+	var res = gFilesystemEnumerateIsFile[LoveFSNormalizePath(path)] || (localStorage && localStorage[gFilesystemPrefix+path] != undefined);
+	return res == true;
+}
+
+function LoveFS_readFile (path) {
+	var file;
+	if (localStorage) { file = localStorage[gFilesystemPrefix+path]; if (file) return file; }
+	UtilAjaxGet(path, function (contents) { file = contents; }, true);
+	return file;
+}
 
 /// init lua api
 function Love_Filesystem_CreateTable (G) {
@@ -7,16 +73,18 @@ function Love_Filesystem_CreateTable (G) {
 
 	G.str['love'].str['filesystem'] = t;
 	
-	t.str['enumerate']				= function () { return NotImplemented(pre+'enumerate'); }
-	t.str['exists']					= function () { return NotImplemented(pre+'exists (no LocalStorage)'); }
+	t.str['enumerate']				= function (path) { return LoveFilesystemEnumerate(path); }
+	
+	t.str['exists']					= function (path) { return [LoveFS_isFile(path) || LoveFS_isDir(path)]; }
+	t.str['isDirectory']			= function (path) { return [LoveFS_isDir(path)]; }
+	t.str['isFile']					= function (path) { return [LoveFS_isFile(path)]; }
+	
 	t.str['getAppdataDirectory']	= function () { return NotImplemented(pre+'getAppdataDirectory'); }
 	t.str['getLastModified']		= function () { return NotImplemented(pre+'getLastModified'); }
 	t.str['getSaveDirectory']		= function () { return NotImplemented(pre+'getSaveDirectory'); }
 	t.str['getUserDirectory']		= function () { return NotImplemented(pre+'getUserDirectory'); }
 	t.str['getWorkingDirectory']	= function () { return NotImplemented(pre+'getWorkingDirectory'); }
 	t.str['init']					= function () { }
-	t.str['isDirectory']			= function () { return NotImplemented(pre+'isDirectory (no LocalStorage)'); }
-	t.str['isFile']					= function () { return NotImplemented(pre+'isFile (no LocalStorage)'); }
 	t.str['lines']					= function () { return NotImplemented(pre+'lines'); }
 	t.str['load']					= function (path) { return [function () { return RunLuaFromPath(path); }]; } // quick&dirty
 	t.str['mkdir']					= function () { return NotImplemented(pre+'mkdir'); }
@@ -27,53 +95,22 @@ function Love_Filesystem_CreateTable (G) {
 	t.str['setIdentity']			= function () { return NotImplemented(pre+'setIdentity (no LocalStorage)'); }
 	t.str['setSource']				= function () { return NotImplemented(pre+'setSource'); }
 	t.str['write']					= function (filename, data) { return NotImplemented(pre+"write (no LocalStorage)"); }
-
+	
+	
 	if (localStorage)
 	{
-		function isDirectory(name)
-		{
-			return (name.substr(name.length-1) == "/"); // Directory name
-		}
-		function isFile(name)
-		{
-			return (localStorage[gFilesystemPrefix+name] != undefined);
-		}
-		function readFile(name)
-		{
-			var file = localStorage[gFilesystemPrefix+name];
-			if (!file)
-			{
-				UtilAjaxGet(name, function (contents)
-				{
-					file = contents;
-				}, true);
-			}
-			return file;
-		}
 		t.str['write']					= function (filename, data)
 		{
 			localStorage[gFilesystemPrefix+filename] = data;
 		}
 		t.str['read']                                   = function (filename)
 		{
-			return [readFile(filename)];
-		}
-		t.str['exists']                                 = function (name)
-		{
-			return [isFile(name) || isDirectory(name)];
+			return [LoveFS_readFile(filename)];
 		}
 		t.str['setIdentity']                            = function (identity)
 		{
 			if (identity)
 				gFilesystemPrefix = identity + "-";
-		}
-		t.str['isDirectory']                            = function (name)
-		{
-			return [isDirectory(name)];
-		}
-		t.str['isFile']                                 = function (name)
-		{
-			return [isFile(name)];
 		}
 		t.str['remove']                                 = function (name)
 		{
@@ -81,7 +118,7 @@ function Love_Filesystem_CreateTable (G) {
 		}
 		t.str['load']                                   = function (name)
 		{
-			var file = readFile(name);
+			var file = LoveFS_readFile(name);
 			if (file)
 			{
 				try
--- js/love.graphics.js
+++ js/love.graphics.js
@@ -89,8 +89,6 @@ function Love_Graphics_CreateTable (G) {
 		else	o.RenderSelf(x,y,r || 0.0,sx || 1.0,sy || 1.0,ox || 0.0,oy || 0.0);
 		return LuaNil;
 	}
-	// NOTE: setMode: var canvas = document.getElementById(gWebGLCanvasId);
-	// NOTE: setMode: if (canvas) { canvas.width = width; canvas.height = height; }
 	
 	t.str['setMode']			= function (width, height, fullscreen, vsync, fsaa)
 	{
@@ -102,7 +100,8 @@ function Love_Graphics_CreateTable (G) {
 			canvas.width = gScreenWidth;
 			canvas.height = gScreenHeight;
 		}
-		MainPrint("setMode",width, height, fullscreen||false, (vsync == undefined)?true:vsync, fsaa||0); return NotImplemented(pre+'setMode');
+		// fullscreen||false, (vsync == undefined)?true:vsync, fsaa||0
+		return LuaNil;
 	}
 	
 	// TODO : "newImage" overloads
